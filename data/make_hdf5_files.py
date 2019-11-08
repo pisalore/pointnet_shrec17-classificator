b616cb729923d3dcb827cb5902d0da3b20806b42
pisalore <lorenzo.pisaneschi@gmail.com>
2019-11-08 11:52:46 +0100

    hdf5 file generation fixed

--- data/make_hdf5_files.py
+++ data/make_hdf5_files.py
@@ -14,34 +14,63 @@ filenames_testing = [line.rstrip() for line in open("filelist_testing.txt", 'r')
 print((len(filenames_training)))
 print((len(filenames_testing)))
 
-
 f_training = h5py.File("./hdf5_data/data_training.h5", 'w')
 f_testing = h5py.File("./hdf5_data/data_testing.h5", 'w')
 
 a_data_training = np.zeros((len(filenames_training), 2048, 3))
 a_pid_training = np.zeros((len(filenames_training), 2048), dtype = np.uint8)
+a_label_training = np.zeros((len(filenames_training), 1), dtype = np.uint8)
 
 a_data_testing = np.zeros((len(filenames_testing), 2048, 3))
 a_pid_testing = np.zeros((len(filenames_testing), 2048), dtype = np.uint8)
+a_label_testing = np.zeros((len(filenames_testing), 1), dtype = np.uint8)
+
+# ====== GENERATING TRAINING FILES ======
+#========================================
 
 for i in range(0, len(filenames_training)):
     print(filenames_training[i])
     plydata = PlyData.read("./ply_dir/" + filenames_training[i] + ".ply")
     piddata = [line.rstrip() for line in open("./seg_dir/" + filenames_training[i] + ".seg", 'r')]
+    # labeldata = [line.rstrip() for line in open("./label_dir/" + filenames_training[i] + ".seg", 'r')]
     for j in range(0, 2048):
         a_data_training[i, j] = [plydata['vertex']['x'][j], plydata['vertex']['y'][j], plydata['vertex']['z'][j]]
         a_pid_training[i, j] = piddata[j]
+        # a_label_training[i, j] = labeldata[j]
+
+for i in range(0, len(filenames_training)):
+    labeldata = [line.rstrip() for line in open("./label_dir/" + filenames_training[i] + ".seg", 'r')]
+    a_label_training[i] = labeldata[i]
+
 data = f_training.create_dataset("data", data = a_data_training)
 pid = f_training.create_dataset("pid", data = a_pid_training)
+label = f_training.create_dataset("label", data = a_label_training)
 
+# ====== GENERATING TRAINING FILES ======
+#========================================
+
+
+# ====== GENERATING TESTING FILES ======
+#========================================
 
 for i in range(0, len(filenames_testing)):
     plydata = PlyData.read("./ply_dir/" + filenames_testing[i] + ".ply")
     piddata = [line.rstrip() for line in open("./seg_dir/" + filenames_testing[i]  + ".seg", 'r')]
+    # labeldata = [line.rstrip() for line in open("./label_dir/" + filenames_testing[i] + ".seg", 'r')]
     for j in range(0, 2048):
         a_data_testing[i, j] = [plydata['vertex']['x'][j], plydata['vertex']['y'][j], plydata['vertex']['z'][j]]
         a_pid_testing[i, j] = piddata[j]
+        # a_label_testing[i, j] = labeldata[j]
+
+for i in range(0, len(filenames_testing)):
+    labeldata = [line.rstrip() for line in open("./label_dir/" + filenames_testing[i] + ".seg", 'r')]
+    a_label_testing[i] = labeldata[i]
+
 data = f_testing.create_dataset("data", data = a_data_testing)
 pid = f_testing.create_dataset("pid", data = a_pid_testing)
+label = f_testing.create_dataset("label", data = a_label_testing)
+
+#========================================
+#========================================
 
 print('HDF5 files generated.')
--- provider.py
+++ provider.py
@@ -88,6 +88,7 @@ def getDataFiles(list_filename):
     return [line.rstrip() for line in open(list_filename)]
 
 def load_h5(h5_filename):
+    print(h5_filename)
     f = h5py.File(h5_filename)
     data = f['data'][:]
     label = f['label'][:]
