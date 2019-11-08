5e3ba13db8a5e918ec721c8f354191d24614cefd
pisalore <lorenzo.pisaneschi@gmail.com>
2019-11-08 09:43:07 +0100

    Dependencies fixed

--- models/transform_nets.py
+++ models/transform_nets.py
@@ -5,7 +5,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, '../utils'))
-import tf_util
+from utils import tf_util
 
 def input_transform_net(point_cloud, is_training, bn_decay=None, K=3):
     """ Input (XYZ) Transform Net, input is BxNx3 gray image
